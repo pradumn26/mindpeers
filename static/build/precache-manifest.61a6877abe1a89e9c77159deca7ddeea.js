f5b3aa9631553122341f6e18ee1dd176d6d1cce5
pradumn26 <pradumn.26@gmail.com>
2019-06-07 15:47:13 +0530

    prod1 commit

---
+++ static/build/precache-manifest.61a6877abe1a89e9c77159deca7ddeea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbed77a81af09ca1205b5eb1604293bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c752ae919a7c57104b86",
+    "url": "/static/css/main.14afcca4.chunk.css"
+  },
+  {
+    "revision": "f81875043315d2492576",
+    "url": "/static/js/2.f473d4bc.chunk.js"
+  },
+  {
+    "revision": "c752ae919a7c57104b86",
+    "url": "/static/js/main.6020ed41.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
